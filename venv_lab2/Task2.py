284708a9e56836aa400da0778de3c7cb891d677a
Volodymyr Nazarenko <nvovajr9.e@gmail.com>
2024-09-17 22:31:40 +0300

    lab2 fully done

--- venv_lab2/Task2.py
+++ venv_lab2/Task2.py
@@ -1,14 +1,9 @@
 import math
-
 n = 1.0
-result = 0
-while n <= 10:
-    x = 1.0
-    while x < 1.51:
-        result = -1 ** (n + 1) * ((x - 1) ** n / n)
-        print("n = " + n.__round__(3).__str__() + " x = " + x.__round__(3).__str__() + " result = " + result.__str__())
-        x += 0.05
+result = 1.0
+x = 1.0
+while x < 1.50001:
+    result = -1 ** (n + 1) * ((x - 1) ** n / n)
+    print("n = " + n.__round__(5).__str__() + " x = " + x.__round__(5).__str__() + " result = " + result.__round__(5).__str__())
+    x += 0.05
     n += 1
-    
-
-    
