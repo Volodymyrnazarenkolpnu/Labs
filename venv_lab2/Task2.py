c228d9071cc036ef80ca33c4aaaa825647eb57c0
Volodymyr Nazarenko <nvovajr9.e@gmail.com>
2024-09-23 15:48:27 +0300

    updated Task2

--- venv_lab2/Task2.py
+++ venv_lab2/Task2.py
@@ -1,11 +1,23 @@
 n = 1
-result = 1.0
+result_fin = 0.0
+result_cur = 0.0
 x = 1.0
-while x < 1.50001:
-    while result.__abs__() > 0.00001:
-        result = -1 ** (n + 1) * ((x - 1) ** n / n)
-        print("n = " + n.__str__() + " x = " + x.__round__(5).__str__() + " result = " + result.__str__())
+deviation = 0.00001
+
+while x.__round__(2) < 1.5:
+    result_cur = -1 ** (n + 1) * ((x - 1) ** n / n)
+    result_fin += result_cur
+    while result_cur.__abs__() > deviation:
+        result_cur = -1 ** (n + 1) * ((x - 1) ** n / n)
+        result_fin += result_cur        
         n += 1
-    result = 1.0
+    
+    print(
+        "x = " + x.__round__(5).__str__() 
+        + " result = " + result_fin.__str__()
+        )
+    result_cur = 0.0
+    result_fin = 0.0
     x += 0.05
+    x = x.__round__(2)
     n = 1
