47f97dea73c12e47886f9dddb6983a4ea795f2d0
Volodymyr Nazarenko <nvovajr9.e@gmail.com>
2024-11-18 07:41:05 +0200

    improved browser capabilities

--- venv_lab7/Task1.py
+++ venv_lab7/Task1.py
@@ -1,27 +1,51 @@
 """Task1 of lab7, variant 2"""
 
-browser_history = ["gmail", "Amazon", "X", "Fortran", "XXX"]
+browser_history = ["gmail", "Amazon", "X", "Fortran", "Steam", "Oracle"]
 
-CURRENT_WEBSITE = "He"
+website_list = []
 POINTER = 0
-def print_browser(pagename):
-    """shows website page"""
-    print(f"@ /{pagename + ".com"}   x\\ +")
+def print_browser():
+    """prints webpage"""
     print("---------------------------------------------")
     for _i in range(14):
         print("|                                            |")
     print("---------------------------------------------")
+    
+def print_browser_history(pagename):
+    """shows website history"""
+    print(f"@ /{pagename + ".com"}   x\\ +")
+    print_browser()
+
+def print_browser_current_pages():
+    """shows website pages"""
+    _line = "@ "
+    _fits = False
+    while not _fits:
+        _l = _line
+        for page in website_list:
+            _l += f"/{page}.com   \\ "
+        if len(_l) > 50 and len(website_list) > 1:
+            website_list.pop(0)
+        else:
+            break
+    for page in website_list:
+        _line += f"/{page}.com   \\ "
+    _line += "+"
+    print(_line)
+    print_browser()
+
 while True:
     print("What would you like to do? search/history/exit")
     request = input()
     if request == "search":
         search = input()
-        print_browser(search)
+        website_list.append(search)
+        print_browser_current_pages()
         browser_history.insert(0, search)
     elif request == "history":
         POINTER = 0
         while True:
-            print_browser(browser_history[POINTER])
+            print_browser_history(browser_history[POINTER])
             print("next/previous/exit")
             request = input()
             if request == "next":
